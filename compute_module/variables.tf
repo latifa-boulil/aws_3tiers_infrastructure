4afadc2bb90d2c20f71ffedc2995cf463757988f
latifa <latifaboulil@hotmail.fr>
2024-11-06 16:02:26 +0100

    adding compute ressources

--- compute_module/variables.tf
+++ compute_module/variables.tf
@@ -16,11 +16,30 @@ variable "instance_image" {
 variable "instance_type" {
     description = "EC2 Instance type"
     type = string
+    default = "t2.micro"
+}
+
+variable "desired_capacity" {
+    description = "our desired number of ec2 from auto scaling group"
+    type = number
+    default = 2
+}
+
+variable "max_size" {
+    description = "maximum number of ec2 provisioned by auto scaling group"
+    type = number
+    default = 4
+}
+
+variable "min_size" {
+    description = "minimum number of ec2 provisioned by auto scaling group"
+    type = number
+    default = 1
 }
 
 
 #########################################
-# variables inherited from output modules
+# variables inherited from other modules
 #########################################
 
 variable "vpc_id"{
@@ -32,16 +51,22 @@ variable "public_subnet_ids" {
     type = list(string)
 }
 
-variable "private_subnet_ids" {
-    type = list(string)
+# variable "private_subnet_ids" {
+#     type = list(string)
   
-}
+# }
 
 variable "ssh_key" {
     type = string
 }
 
+variable "web_sg" {
+    type = string
+  
+}
 
-
+# variable "app_sg" {
+#     type = string
+# }
 
 
--- vpc_module/main.tf
+++ vpc_module/main.tf
@@ -29,17 +29,17 @@ resource "aws_subnet" "public_subnet" {
     }
 }
 
-resource "aws_subnet" "private_subnet" {
-    for_each = zipmap(var.az, var.private_cidr)
-    vpc_id = aws_vpc.my_vpc.id      
-    availability_zone = each.key
-    cidr_block = each.value 
-    map_public_ip_on_launch = false #no public ip assigned for private subnet         #OK
+# resource "aws_subnet" "private_subnet" {
+#     for_each = zipmap(var.az, var.private_cidr)
+#     vpc_id = aws_vpc.my_vpc.id      
+#     availability_zone = each.key
+#     cidr_block = each.value 
+#     map_public_ip_on_launch = false #no public ip assigned for private subnet         #OK
 
-    tags = {
-        Name = "app_tier_${var.environment}_${each.key}" # back_end 
-    }
-}
+#     tags = {
+#         Name = "app_tier_${var.environment}_${each.key}" # back_end 
+#     }
+# }
 
 # resource "aws_subnet" "database_subnet" {
 #     for_each = zipmap(var.az, var.database_cidr)
@@ -59,49 +59,49 @@ resource "aws_subnet" "private_subnet" {
 # CONNECTIVITY RESSOURCES
 ############################################
 
-# resource "aws_internet_gateway" "internet_gateway" {                          # OK
-#   vpc_id = aws_vpc.my_vpc.id
-# }
+resource "aws_internet_gateway" "internet_gateway" {                          # OK
+  vpc_id = aws_vpc.my_vpc.id
+}
 
 
 # -------- PUBLIC SUBNET CONNECTIVITY ------------
 
-# resource "aws_eip" "nat_ip" {                                                   # OK
-#   for_each = toset(var.az)                        
-# }
+resource "aws_eip" "nat_ip" {                                                   # OK
+  for_each = toset(var.az)                        
+}
 
 
-# resource "aws_nat_gateway" "nat_gateway" {
-#   for_each            = aws_subnet.public_subnet
-#   allocation_id       = aws_eip.nat_ip[each.key].id                               # OK 
-#   subnet_id           = each.value.id
-#   connectivity_type   = "public"
+resource "aws_nat_gateway" "nat_gateway" {
+  for_each            = aws_subnet.public_subnet
+  allocation_id       = aws_eip.nat_ip[each.key].id                               # OK 
+  subnet_id           = each.value.id
+  connectivity_type   = "public"
 
-#   tags = {
-#     Name = "nat_gateway_${each.key}"
-#   }
-#   depends_on = [aws_eip.nat_ip]  # cannot create nat_gateway without elastic IP adress error thrown
-# }
+  tags = {
+    Name = "nat_gateway_${each.key}"
+  }
+  depends_on = [aws_eip.nat_ip]  # cannot create nat_gateway without elastic IP adress error thrown
+}
 
 
-# # Route Table for Public Subnet (Front End) with NAT Gateway
-# resource "aws_route_table" "public_rt" {
-#   for_each = aws_subnet.public_subnet
-#   vpc_id = aws_vpc.my_vpc.id                                                     # OK
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.internet_gateway.id
-#   }
-#   tags = {
-#     Name = "public-route-table"
-#   }
-# }
+# Route Table for Public Subnet (Front End) with NAT Gateway
+resource "aws_route_table" "public_rt" {
+  for_each = aws_subnet.public_subnet
+  vpc_id = aws_vpc.my_vpc.id                                                     # OK
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
 
-# resource "aws_route_table_association" "publicRTlink" {
-#   for_each  = aws_subnet.public_subnet
-#   subnet_id = each.value.id                                                         # OK
-#   route_table_id = aws_route_table.public_rt[each.key].id
-# }
+resource "aws_route_table_association" "publicRTlink" {
+  for_each  = aws_subnet.public_subnet
+  subnet_id = each.value.id                                                         # OK
+  route_table_id = aws_route_table.public_rt[each.key].id
+}
 
 
 # ------PRIVATE SUBNET CONNECTIVITY ( APP TIERS )--------
